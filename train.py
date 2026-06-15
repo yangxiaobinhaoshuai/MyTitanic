15511d3fd426ed63c90a108a88c531fc2dafd1dc
yangxiaobin01 <yangxiaobin01@zuoyebang.com>
2026-06-15 16:25:16 +0800

    Submit submission file.

--- train.py
+++ train.py
@@ -1,11 +1,31 @@
+from pathlib import Path
+
+import pandas as pd
+from sklearn.metrics import accuracy_score
+from xgboost import XGBClassifier
+
 from data.data_process import get_processed_data
 from my_logger import logger
-from xgboost import XGBClassifier
-from sklearn.metrics import accuracy_score
-import os
-import pandas as pd
 
 lr = 0.1
+
+
+# Useless for now
+def write_sub_csv(test_passenger_id: pd.Series, y_test: pd.Series, force: bool = False):
+
+    submission_path: Path = Path("submission.csv")
+
+    if not force:
+        if submission_path.exists():
+            logger.info("submission file already exist, skip override")
+            return
+
+    #  Force override branch
+    logger.info("New or override submission file.")
+
+    res_dict = {"PassengerId": test_passenger_id, "Survived": y_test}
+
+    pd.DataFrame(res_dict).to_csv(submission_path, index=False)
 
 
 def train():
@@ -16,7 +36,7 @@ def train():
         X_val,
         y_val,
         X_test,
-        test_passender_id,
+        test_passenger_id,
     ) = get_processed_data()
 
     logger.info(f"X_train shape:{X_train.shape}")
@@ -26,49 +46,23 @@ def train():
     model = XGBClassifier(n_estimators=100, max_depth=3, learning_rate=lr)
     model.fit(X_train, y_train)
     pred_val = model.predict(X_val)
+
     acc = accuracy_score(y_val, pred_val)
     logger.info(f"val acc : {acc}")
 
     pred_test = model.predict(X_test)
-    logger.info(f"test acc : {pred_test}")
+    logger.info(f"y_test prediction: {pred_test}")
+
+    # Write to csv
+    write_sub_csv(test_passenger_id, pred_test, force=True)
 
 
-# TODO
-# train()
 
 
-#  TODO
-
-# def convert_to_map(
-#     y_test: pd.Series, test_passenger_id: pd.Series
-# ) -> dict[int, int]: 
-#     map = {}
-#     for idx,y in y_test.items():
-#         map[test_passenger_id.iloc[idx]] = y
-
-#         return map
-        
+# Redundant for now
+def convert_to_map(y_test: pd.Series, test_passenger_id: pd.Series) -> dict[int, int]:
+    return dict(zip(test_passenger_id, y_test))
 
 
-# TODO
-def write_sub_csv(map: dict[int, int], force: bool = False):
-
-    if not force:
-        out_file_name = "submission.csv"
-        cur = os.path.curdir
-        out_file = os.path.join(cur, out_file_name)
-
-        if out_file:
-            logger.info("submission file alread exit, skip override")
-            return
-
-    else:
-        #  Force override branch
-        logger.info(f"force override submission file, map len: ${len(map)}")
-        submissions = pd.DataFrame(
-            {
-                "PassengerId": map.keys,
-                "Survived": map.values,
-            }
-        )
-        submissions.to_csv("submission.csv", index=False)
+if __name__ == "__main__":
+    train()
